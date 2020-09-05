f6cbf71ad958e2888476b97e2c45c83ba75014f5
Dimitri <dimitrimachines@gmail.com>
2020-09-04 22:32:13 -0400

    first commit

---
+++ src/Components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <div className="main">Hello</div>
+      <div className="main-2">Hello</div>
+      <div className="projects">Hello</div>
+        <div className="contact">Hello</div>
+    </div>
+  );
+};
+
+export default Main;
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container">
+      <div className="sidebar">
+        <div className="sidebar-content">lorem ipsum</div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
