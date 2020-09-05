552b0f62e9ead5c4f4709d8a347156c52ec62cb4
Dimitri <dimitrimachines@gmail.com>
2020-09-05 00:51:35 -0400

    sidebar details

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,7 +4,15 @@ const Sidebar = () => {
   return (
     <div className="sidebar-container">
       <div className="sidebar">
-        <div className="sidebar-content">lorem ipsum</div>
+        <div className="sidebar-content">
+          <div className="name">
+            <p className="full-name">Dimitri Pierre Louis</p>
+            <p className="info">
+              Software Developer in Boston, MA
+            </p>
+          </div>
+          <br />
+        </div>
       </div>
     </div>
   );
